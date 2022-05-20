1b5e9a27e49a3019d6cc30138bf0b448e33e73b8
shihab <game123ahmed@gmail.com>
2022-05-20 06:14:47 +0600

    new code added

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,24 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import projectsData from './projectsData'
+import TailwindP from './TailwindP'
 
 
 const Projects=()=>{
+
+    const [pdata,psetData] = useState(projectsData)
     return(
         <>
-        <div className="max-w-sm bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-900">
-        <a href="#">
-        <img className=" rounded-t-xl" src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&raw_url=true&q=60&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1yZWxhdGVkfDN8fHxlbnwwfHx8fA%3D%3D&auto=format&fit=crop&w=500" alt=""/>
-         </a>
-        <div className="p-3">
-        <a href="#">
-        <h5 className="mb-2 text-sm font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy <br/> technology</h5>
-        </a>
-       <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise.</p>
-       <a href="#" className ="inline-flex items-center py-2 px-3 text-sm font-medium text-center text-white bg-blue-500 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-800 dark:focus:ring-blue-800">
-       Visite
-       <svg className="ml-2 -mr-1 w-4 h-4" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-      </a>
-      </div>
-       </div>
+        {
+            pdata.map((newData)=>{
+                return <TailwindP key={newData} {...newData}   />
+
+            })
+        }
 
         </>
     )
---
+++ src/components/Projects/projectsData.js
@@ -0,0 +1,29 @@
+let projectsData = [
+    {
+        id:1,
+        title: "Calculator App",
+        para:'So in this blog, we will discuss',
+        image:"https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=869"
+    },
+    {
+        id:2,
+        title: "Messenge",
+        para:'The next React Project Ideas for Beginners on which you can work',
+        image:"https://images.unsplash.com/photo-1526253038957-bce54e05968e?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870"
+    },
+    {
+        id:1,
+        title: "Gaming App",
+        para:'Social Media App. Future of Social Media Marketing in India',
+        image:"https://images.unsplash.com/photo-1547658719-da2b51169166?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464"
+    },
+    {
+        id:1,
+        title: "Social Media App",
+        para:'Twitter, are prime examples of sophisticated apps.',
+        image:"https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-1.2.1&raw_url=true&q=80&fm=jpg&crop=entropy&cs=tinysrgb&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=455"
+    }
+
+]
+
+export default projectsData
