832d6ee664ef65d6b5963c5707907da83a50466b
shihab <game123ahmed@gmail.com>
2022-05-22 09:23:23 +0600

    new code added

--- src/LandingPage.js
+++ src/LandingPage.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import Home from './components/Home/Home'
 import Navbar from './components/Navbar/Navbar'
-import Projects from './components/Projects/Projects'
-import './landingPage.css'
+
+
 
 
 const LandingPage=()=>{
@@ -10,10 +10,8 @@ const LandingPage=()=>{
         <div>
             <Navbar/>
             <Home/>
-           <div className='projects__sections'>
-             <Projects/>
-             
-           </div>
+         
+           
         </div>
     )
 }
