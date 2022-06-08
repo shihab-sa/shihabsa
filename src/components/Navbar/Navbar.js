ea3a2ccda4beea64329deb137336d254532d3b9c
shihab <game123ahmed@gmail.com>
2022-06-08 17:57:23 +0600

    new code added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,26 +2,62 @@ import React from 'react'
 import './navbar.css'
 
 
-
-
 const Navbar=()=>{
-    return(
-            <nav>
-               <div className="nav__container container">
-            <div className="nav__menu__list">
-                <ul>
-                     <li><a href="#">Shihab.</a></li>
-                </ul>
 
-                {/* <div className='nav__dark__mode'>
-                   <Toggle/>
-                          
-                 </div>  */}
-               
-              </div>
-                
-        </div>
-    </nav>
+    
+
+
+return(
+            <nav>
+               <div className='nav__container container'>
+                   {/* <a href='#' className='nav__logo'>Shihab</a> */}
+                   <div className='nav__menu__list'>
+                       <ul className='nav__list'>
+
+                               {/* 1  */}
+                               <li className='list__item'>
+                                   <a href='#home' className='nav__link'>
+                                   <i class='bx bx-home'></i>
+                                   </a>
+                               </li>
+                               
+                               {/* 2 */}
+                                {/* 1  */}
+                                <li className='list__item'>
+                                   <a href='#about' className='nav__link'>
+                                   <i class='bx bx-user-voice'></i>
+                                   </a>
+                               </li>
+
+                               {/* 3  */}
+                                {/* 1  */}
+                                <li className='list__item'>
+                                   <a href='#skill' className='nav__link'>
+                                   <i class='bx bx-book'></i>
+                                   </a>
+                               </li>
+
+                               {/* 4  */}
+                                {/* 1  */}
+                                <li className='list__item'>
+                                   <a href='#work' className='nav__link'>
+                                   <i class='bx bx-briefcase'></i>
+                                   </a>
+                               </li>
+
+                               {/* 5  */}
+
+                               <li className='list__item'>
+                                   <a href='#contact' className='nav__link'>
+                                   <i class='bx bxs-contact'></i>
+                                   </a>
+                               </li>
+                       </ul>
+
+                   </div>
+               </div>
+
+            </nav>
                 
         
     )
